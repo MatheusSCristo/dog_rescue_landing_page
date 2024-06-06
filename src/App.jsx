5f0de6819eede4271cac79a82c4d3c4fbaaa8989
Matheus <cristo.matheus@outlook.com>
2024-06-06 14:05:08 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Home from "./Home";
+import styles from "./app.module.scss";
+
+
+function App() {
+  return (
+    <main className={styles.main_container}>
+      <Home />
+    </main>
+  );
+}
+
+export default App;
